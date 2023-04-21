e8bf092a6f3d35d23f7d50ef54b95086809d09f6
alexandrekcosta <xandykelvin@gmail.com>
2023-04-21 00:31:04 -0300

    UPDATE:New files have been uploaded

---
+++ BanknoteWithdraw/BanknoteWithdraw/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+Console.WriteLine("ENTER A QUANTITY TO WITHDRAW: ");
+double quantity = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+double _100bn = quantity / 100;
+double _50bn = quantity / 50;
+double _20bn = quantity / 20;
+double _10bn = quantity / 10;
+double _5bn = quantity / 5;
+double _2bn = quantity / 2;
+
+Console.WriteLine(" ");
+Console.WriteLine("TOTAL QUANTITY: {0}", quantity.ToString("F2",CultureInfo.InvariantCulture));
+Console.WriteLine("100 REAIS BANKNOTES:{0}", _100bn.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("50 REAIS BANKNOTES: {0}", _50bn.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("20 REAIS BANKNOTES: {0}", _20bn.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("10 REAIS BANKNOTES: {0}", _10bn.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("5 REAIS BANKNOTES: {0}", _5bn.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("2 REAIS BANKNOTES: {0}", _2bn.ToString("F2", CultureInfo.InvariantCulture));
---
+++ TableSimple/TableSimple/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input a 1st number: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Input a 2nd number: ");
+int n2 = int.Parse(Console.ReadLine());
+
+int sum = n1 + n2;
+int sub = n1 - n2;
+int multiplication = n1 * n2;
+int division = n1 / n2;
+
+Console.WriteLine(" ");
+Console.WriteLine("SUM: {0} + {1} = {2}", n1, n2, sum);
+Console.WriteLine("SUBTRACTION: {0} - {1} = {2}", n1, n2, sub);
+Console.WriteLine("MULTIPLICATION: {0} * {1} = {2}", n1, n2, multiplication);
+Console.WriteLine("DIVISION: {0} / {1} = {2}", n1, n2, division);
