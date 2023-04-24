896ec0265498f87439f805fbdb7f044b1b6fd1be
alexandrekcosta <xandykelvin@gmail.com>
2023-04-24 19:31:48 -0300

    UPDATE:Editions have been made on code files

--- cities/cities/Program.cs
+++ cities/cities/Program.cs
@@ -5,23 +5,30 @@ int population;
 int areakm2;
 double gdp;
 double gdpcapita;
+int PercentValue;
 
 Console.WriteLine("Enter name of city: ");
 name = Console.ReadLine();
 Console.WriteLine("Enter name of country where city is located in: ");
 country = Console.ReadLine();
-Console.WriteLine("Enter population of city: ");
+Console.WriteLine("Enter population of {0}: ",name);
 population = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter area km per square of city: ");
+Console.WriteLine("Enter area km per square of {0}: ",name);
 areakm2 = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter GDP of {0}: ",country);
 gdp = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-Console.WriteLine("Enter GDP per capita of city: ");
+Console.WriteLine("Enter GDP per capita of {0}: ",name);
 gdpcapita = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Enter percent value: ");
+PercentValue = int.Parse(Console.ReadLine());
 
-if(population < 20000 && gdpcapita < 2000.0 && areakm2 < 100000.0){
 
+
+if(population < 20000 && gdpcapita < 2000 && areakm2 < 100000){
+    double FundValue = gdp - (gdp * PercentValue / 100);
+    double t = FundValue - gdp;
     Console.WriteLine("THIS CITY IS ELEGIBLE FOR EXTRA-MUNICIPAL FUND");
+    Console.WriteLine("EXTRA-MUNICIPAL FUND VALUE:{0} ",t.ToString("F2",CultureInfo.InvariantCulture));
 
 }else{
 
